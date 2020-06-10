7013ec99fd7e85e74bfd3ab600ceef33dc61f445
Sebastien Le Maguer <lemagues@tcd.ie>
2020-06-10 16:16:45 +0100

    [artist] add: CD order management

--- ma2oa-artist.el
+++ ma2oa-artist.el
@@ -27,12 +27,16 @@
 
 ;;; Code:
 
+(require 'ma2oa)
 (require 'org)
 (require 'org-element)
+(require 'om)
 
 
 
-(defvar artist-map-filename (format "%s/artist-map.cache" user-emacs-directory))
+(defvar artist-map-filename (format "%s/artist-map.cache" user-emacs-directory)
+  "The cache containing the hashtable which key are the favorite
+  artists and the corresponding value is the priority.")
 
 (defun ma2oa-load-artists-map ()
   "Loading the artist priority map stored in FiLENAME."
@@ -57,32 +61,75 @@
 
 
 
-(defun ma2oa-fill-artists-priority-map (filename)
-  "2019-01-14"
+(defcustom ma2oa-to-order-target-file "~/test.org"
+  "File containing the order list in an org-mode format.")
+
+(defcustom ma2oa-to-order-root-node "VVV"
+  "Headline value whose children are the CDs to order.")
+
+(defvar ma2oa-cd-to-flush '())
+
+(defun ma2oa~generate-node (level cd)
+  (om-build-headline! :title-text (format "%s - %s" (ma2oa-entry-artist cd) (ma2oa-entry-album cd))
+                      :level level
+                      :tags (list (ma2oa-entry-type cd))
+                      :todo-keyword "RELEASE"
+                      :section-children (list
+                                         ;; (om-build-planning! :scheduled '(2018 1 1))
+                                         (om-build-property-drawer! (list (intern "GENRE")
+                                                                          (intern (ma2oa-entry-genre cd)))
+                                                                    '(CATEGORY RELEASE))
+                                         )))
+
+(defun ma2oa~children-headline-set (children)
+  (let* ((headlines))
+    (dolist (elt children)
+      (when (eq (org-element-type elt) 'headline)
+        (add-to-list 'headlines (org-element-property :raw-value elt))))
+      (delete-dups headlines)))
+
+(defun ma2oa~add-release (cur-node)
+  "Add release as a children node to CUR-NODE."
+  (let* ((level (+ 1 (org-element-property :level cur-node)))
+         (children (om-get-children cur-node))
+         (headline-set (ma2oa~children-headline-set children)))
+    (dolist (cd ma2oa-cd-to-flush)
+      (unless (member (format "%s - %s" (ma2oa-entry-artist cd) (ma2oa-entry-album cd)) headline-set)
+        (setq children (append children (list (ma2oa~generate-node level cd))))))
+    (om-set-children children cur-node)))
+
+(defun ma2oa~update-cd (cur-node)
+  "Recursive browsing and updating of the tree which root is given by CUR-NODE in order to add the RELEASE."
+  (if (string= (org-element-property :raw-value cur-node) ma2oa-to-order-root-node)
+      (setq cur-node (ma2oa~add-release cur-node))
+    (om-map-children* (--map (ma2oa~update-cd it) it) cur-node))
+  cur-node)
+
+(defun ma2oa-update-order-list ()
+  "Update the CD order list."
   (interactive)
+  (with-current-buffer (find-file ma2oa-to-order-target-file)
+    (let* ((todo-tree (org-element-parse-buffer)))
+      ;; We don't need the content, everything will be replaced!
+      (erase-buffer)
 
-  (with-current-buffer (find-file filename)
-    (org-map-entries
-     (lambda ()
-       (let ((cur-artist (replace-regexp-in-string " (.*" ""
-                                                   (nth 4 (org-heading-components)))))
-         (unless (ht-get ma2oa-favorite-artists cur-artist)
-           (ht-set ma2oa-favorite-artists cur-artist 'normal)))))
-    (kill-this-buffer)))
+      ;; Remove the first useless nodes
+      (pop todo-tree)
+      (pop todo-tree)
 
-(ma2oa-fill-artists-priority-map "~/shared/Dropbox/music/cd.org")
+      ;; Update the todo list
+      (dolist (elt todo-tree)
+        (when (eq (org-element-type elt) 'headline)
+          (setq elt (om-map-children* (--map (ma2oa~update-cd it) it) elt)))
+        (insert (om-to-string elt)))
 
-
+      ;; Flush
+      (save-buffer)
+      (setq ma2oa-cd-to-flush '())
 
-;; (defun ma2oa-manage-favorite (entry)
-;;   (ma2oa-favorite-alert entry))
-
-;; (setq ma2oa-favorite-handle 'ma2oa-manage-favorite)
-;; ;; ma2oa~format-entry
-
-
-;; (with-current-buffer (find-file "~/shared/pCloudDrive/org/todo/todo.org")
-;;   (org-element-parse-buffer))
+      ;; Switch back to the previous buffer
+      (switch-to-buffer (other-buffer (current-buffer) 1))
+      )))
 
 
 
